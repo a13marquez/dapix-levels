e71c2159d39cdae105595407719155e41e754d51
Álvaro Márquez <marquezalvaro13@gmail.com>
2018-02-18 11:31:48 +0100

    Remove unnecesary files

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # dapix-levels2
 
 > Level editor
@@ -24,19 +23,3 @@ npm test
 ---
 
 This project was generated with [electron-vue](https://github.com/SimulatedGREG/electron-vue)@[1c165f7](https://github.com/SimulatedGREG/electron-vue/tree/1c165f7c5e56edaf48be0fbb70838a1af26bb015) using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://simulatedgreg.gitbooks.io/electron-vue/content/index.html).
-=======
-# Dapix Levels
-Level editor
-
-## Introduction
-This project is based on Electron and VueJS, using EaselJS for manage the canvas element.
-More info on the web: https://kloder-games.github.io/dapix-levels/
-
-## Current state
-
-Currently the state of the project is Alpha o early, but some testers are now using it on his projects, so here is the start of this beautiful work.
-If you want to use it and miss some help information to work with it write to me for a better documentation.
-Thanks!
-
-## License MIT
->>>>>>> 739c595db1d22212269627b904401bc9334d5a81
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,27 +1,13 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 Vue.config.devtools = false
 Vue.config.productionTip = false
 
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context('./specs', true, /\.spec$/)
-=======
-// Add this to make Electron code run properly. To be perfectly
-// honest, I'm not sure why this is necessary.
-window.process = window.parent.process
-window.require = window.parent.require
-
-// require all test files (files that ends with .spec.js)
-var testsContext = require.context('./specs', true, /\.spec$/)
->>>>>>> 739c595db1d22212269627b904401bc9334d5a81
 testsContext.keys().forEach(testsContext)
 
 // require all src files except main.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-<<<<<<< HEAD
 const srcContext = require.context('../../src/renderer', true, /^\.\/(?!main(\.js)?$)/)
-=======
-var srcContext = require.context('../../app', true, /^\.\/(?!(background|main)(\.js)?$)/)
->>>>>>> 739c595db1d22212269627b904401bc9334d5a81
 srcContext.keys().forEach(srcContext)
